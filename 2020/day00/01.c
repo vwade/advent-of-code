a4e8c646695a6ce07cf2f09b8b27ae43e40ae6c0
Victor Wade <44330039+vwade@users.noreply.github.com>
2020-12-01 21:39:47 -0800

    Create 01.c
    1 of 2 for Day 00

---
+++ 2020/day00/01.c
@@ -0,0 +1,20 @@
+#include "libft.h"
+
+int
+get_next_line(int fd, char **line)
+{
+	
+}
+
+void
+expense_report(char *input)
+{
+	int		fd = open(input, O_RDONLY);
+	char	*line;
+	int		**report =
+	
+	while (get_next_line(fd, &line) > 0)
+	{
+		
+	}
+}
